785ac015e0433ea80f639dba1408bf830e648b94
Carrington <david.carrington@u.mal.utc.com>
2018-07-09 15:50:28 +0000

    Completed REST API exercise

---
+++ rest-api-exercise/routes/posts.js
@@ -0,0 +1,23 @@
+const store = require('../store/store.js')
+
+module.exports = {
+    getPosts(req, res) {
+        res.status(200).send(store.posts)
+    },
+    addPost(req, res) {
+        let newPost = req.body
+        let postId = store.posts.length
+        store.posts.push(newPost)
+        res.status(201).send({
+            postId: postId
+        })
+    },
+    updatePost(req, res) {
+        store.posts[req.params.postId] = Object.assign(store.posts[req.params.postId], req.body)
+        res.status(200).send(req.store.posts[req.params.postId])
+    },
+    removePost(req, res) {
+        store.posts.splice(req.params.postId, 1)
+        res.status(204).send()
+    }
+}
